e99c4bf856882664d3943443732aa7c0aca2fddb
Anton Maliutenko <maluk.tony@gmail.com>
2022-11-06 16:22:01 +0200

    Created new classes Cardholder.java were updated

---
+++ src/Cardholder.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Cardholder {
+
+    private String firstName; //the user's first name
+
+    private String lastName; //the user's last name
+
+    private String idNum; //the user's ID number
+
+    private byte pinPassword[]; //4 digits of the user's PIN password
+
+    private ArrayList<Account> accounts; //the list of user's accounts
+
+}
