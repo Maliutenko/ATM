b39898c4a683107ec596c906fc495ca17a90ca9b
Anton Maliutenko <maluk.tony@gmail.com>
2022-11-09 14:13:56 +0200

    Variables to Transaction.java have added

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,2 +1,13 @@
+import java.util.Date;
+
 public class Transaction {
+
+    private double amount; //the amount of the transaction;
+
+    private Date timestamp; //the date and time of the transaction;
+
+    private String memo; //the memo with description of the transaction;
+
+    private Account inAccount;//information about an account;
+
 }
